475ab8cf6f3931d8bc5e54e157e26810189ee713
Joel Flores <joelf.-@live.com>
2021-12-02 15:21:20 -0300

    Add plugin to support older browser and minify html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { minifyHtml } from 'vite-plugin-html'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    minifyHtml(),
+    legacy({
+      ignoreBrowserslistConfig: true,
+    }),
+  ]
 })
